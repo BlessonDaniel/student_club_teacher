f3486a438cade391cd4e0f2eed16a1eb660a2a21
BlessonDaniel <61042237+BlessonDaniel@users.noreply.github.com>
2021-07-23 12:04:30 +0530

    Update db.js

--- stud_club/app/utils/db.js
+++ stud_club/app/utils/db.js
@@ -3,7 +3,7 @@ const knex= require('knex')({
     connection: {
         host     : '127.0.0.1',
         user     : 'database1',
-        password : 'faustina23',
+        password : 'pw',
         database : 'sample',
         charset  : 'utf8'
       }
